1cdb790bd808e93a2e7c7d3ecd60c8d939b13cc3
TrickEye <20373866@buaa.edu.cn>
2024-08-27 22:32:34 +0800

    1

--- scripts/ub_check_ubuntu.py
+++ scripts/ub_check_ubuntu.py
@@ -32,25 +32,31 @@ def ub_check(mainfile, auxfiles, examples, skiptest):
         return ['SKIPPED']
     
     # 编译指令和编译产物
-    compile_commands = [f'clang++ -std=c++17 -O0 {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.Clang.O0',
-                        f'clang++ -std=c++17 -O2 {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.Clang.O2',
-                        f'clang++ -std=c++17 -O3 {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.Clang.O3',
-                        f'g++-9 -std=c++17 -O0 {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.GCC9.O0',
-                        f'g++-9 -std=c++17 -O2 {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.GCC9.O2',
-                        f'g++-9 -std=c++17 -O3 {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.GCC9.O3',
-                        f'g++-10 -std=c++17 -O0 {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.GCC10.O0',
-                        f'g++-10 -std=c++17 -O2 {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.GCC10.O2',
-                        f'g++-10 -std=c++17 -O3 {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.GCC10.O3',
+    compile_commands = [f'clang++ -std=c++14 -O0 {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.Clang.O0',
+                        f'clang++ -std=c++14 -O2 {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.Clang.O2',
+                        f'clang++ -std=c++14 -O3 {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.Clang.O3',
+                        f'g++-11 -std=c++14 -O0 -static {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.CPP14.GCC11.O0',
+                        f'g++-11 -std=c++14 -O2 -static {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.CPP14.GCC11.O2', # NOI 2023
+                        f'g++-11 -std=c++14 -O3 -static {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.CPP14.GCC11.O3',
+                        f'g++-11 -std=gnu20 -O0 -static {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.GNU20.GCC11.O0',
+                        f'g++-11 -std=gnu20 -O2 -static {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.GNU20.GCC11.O2', # ACM ICPC 2023
+                        f'g++-11 -std=gnu20 -O3 -static {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.GNU20.GCC11.O3', 
+                        f'g++-13 -std=c++14 -O0 -static {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.CPP14.GCC13.O0',
+                        f'g++-13 -std=c++14 -O2 -static {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.CPP14.GCC13.O2',
+                        f'g++-13 -std=c++14 -O3 -static {" ".join(auxfiles)} -o {mainfile.split(".")[0]}.CPP14.GCC13.O3',
     ]
     compile_products = [f'{mainfile.split(".")[0]}.Clang.O0',
                         f'{mainfile.split(".")[0]}.Clang.O2',
                         f'{mainfile.split(".")[0]}.Clang.O3',
-                        f'{mainfile.split(".")[0]}.GCC9.O0',
-                        f'{mainfile.split(".")[0]}.GCC9.O2',
-                        f'{mainfile.split(".")[0]}.GCC9.O3',
-                        f'{mainfile.split(".")[0]}.GCC10.O0',
-                        f'{mainfile.split(".")[0]}.GCC10.O2',
-                        f'{mainfile.split(".")[0]}.GCC10.O3',
+                        f'{mainfile.split(".")[0]}.CPP14.GCC11.O0',
+                        f'{mainfile.split(".")[0]}.CPP14.GCC11.O2',
+                        f'{mainfile.split(".")[0]}.CPP14.GCC11.O3',
+                        f'{mainfile.split(".")[0]}.GNU20.GCC11.O0',
+                        f'{mainfile.split(".")[0]}.GNU20.GCC11.O2',
+                        f'{mainfile.split(".")[0]}.GNU20.GCC11.O3',
+                        f'{mainfile.split(".")[0]}.CPP14.GCC13.O0',
+                        f'{mainfile.split(".")[0]}.CPP14.GCC13.O2',
+                        f'{mainfile.split(".")[0]}.CPP14.GCC13.O3',
     ]
 
     return_status = {}
