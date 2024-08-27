ced36859b0a4bb84997328e06451fae9535f8b45
TrickEye <20373866@buaa.edu.cn>
2024-08-27 20:44:37 +0800

    1

--- scripts/ub_check_riscv.py
+++ scripts/ub_check_riscv.py
@@ -109,3 +109,6 @@ for mainfile, auxfile, example, skiptest in zip(mainfiles, auxfiles, examples, s
 with open('github_step_summary.txt', 'w') as f:
     f.write(f'# TOTAL {len(mainfiles)} TESTS, {cnt_ac} ACCEPTED, {cnt_error} may include UB\n\n')
     print(f'::group::TOTAL {len(mainfiles)} TESTS, {cnt_ac} ACCEPTED, {cnt_error} may include UB\n::endgroup::')
+
+if cnt_error:
+    exit(1)
