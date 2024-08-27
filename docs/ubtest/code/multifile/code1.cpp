7f71426139891707cc176fdad62dd7e9d8bd826b
TrickEye <20373866@buaa.edu.cn>
2024-08-27 20:15:21 +0800

    bugfix

--- docs/ubtest/code/multifile/code1.cpp
+++ docs/ubtest/code/multifile/code1.cpp
@@ -5,7 +5,7 @@ extern int fibonacci(int n);
 int main() {
     int n;
     scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         printf("%d\n", fibonacci(i));
     }
     return 0;
