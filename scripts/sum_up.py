550ad023c70e2fe738721404b6df7b9a8646b2bc
TrickEye <20373866@buaa.edu.cn>
2024-08-31 20:30:11 +0800

    Update sum_up.py

--- scripts/sum_up.py
+++ scripts/sum_up.py
@@ -29,44 +29,44 @@ with open(os.environ.get('GITHUB_STEP_SUMMARY'), 'w') as f:
         for line in OUTPUT_MACOS[key]:
             print(f'- {line}: {OUTPUT_MACOS[key][line]}')
             f.write(f'- {line}: {OUTPUT_MACOS[key][line]}\n')
-            if 'CE' in OUTPUT_UBUNTU[key][line]:
+            if 'CE' in OUTPUT_MACOS[key][line]:
                 this_file_looks_odd = True
-            if 'RE' in OUTPUT_UBUNTU[key][line]:
+            if 'RE' in OUTPUT_MACOS[key][line]:
                 this_file_looks_odd = True
-            if 'WA' in OUTPUT_UBUNTU[key][line]:
+            if 'WA' in OUTPUT_MACOS[key][line]:
                 this_file_looks_odd = True
         print(f'x86_64 Alpine with MUSL')
         f.write(f'x86_64 Alpine with MUSL\n')
         for line in OUTPUT_ALPINE[key]:
             print(f'- {line}: {OUTPUT_ALPINE[key][line]}')
             f.write(f'- {line}: {OUTPUT_ALPINE[key][line]}\n')
-            if 'CE' in OUTPUT_UBUNTU[key][line]:
+            if 'CE' in OUTPUT_ALPINE[key][line]:
                 this_file_looks_odd = True
-            if 'RE' in OUTPUT_UBUNTU[key][line]:
+            if 'RE' in OUTPUT_ALPINE[key][line]:
                 this_file_looks_odd = True
-            if 'WA' in OUTPUT_UBUNTU[key][line]:
+            if 'WA' in OUTPUT_ALPINE[key][line]:
                 this_file_looks_odd = True
         print(f'x86_64 Windows 10')
         f.write(f'x86_64 Windows 10\n')
         for line in OUTPUT_WINDOWS[key]:
             print(f'- {line}: {OUTPUT_WINDOWS[key][line]}')
             f.write(f'- {line}: {OUTPUT_WINDOWS[key][line]}\n')
-            if 'CE' in OUTPUT_UBUNTU[key][line]:
+            if 'CE' in OUTPUT_WINDOWS[key][line]:
                 this_file_looks_odd = True
-            if 'RE' in OUTPUT_UBUNTU[key][line]:
+            if 'RE' in OUTPUT_WINDOWS[key][line]:
                 this_file_looks_odd = True
-            if 'WA' in OUTPUT_UBUNTU[key][line]:
+            if 'WA' in OUTPUT_WINDOWS[key][line]:
                 this_file_looks_odd = True
         print(f'RISC-V64 Ubuntu 22.04')
         f.write(f'RISC-V64 Ubuntu 22.04\n')
         for line in OUTPUT_RV[key]:
             print(f'- {line}: {OUTPUT_RV[key][line]}')
             f.write(f'- {line}: {OUTPUT_RV[key][line]}\n')
-            if 'CE' in OUTPUT_UBUNTU[key][line]:
+            if 'CE' in OUTPUT_RV[key][line]:
                 this_file_looks_odd = True
-            if 'RE' in OUTPUT_UBUNTU[key][line]:
+            if 'RE' in OUTPUT_RV[key][line]:
                 this_file_looks_odd = True
-            if 'WA' in OUTPUT_UBUNTU[key][line]:
+            if 'WA' in OUTPUT_RV[key][line]:
                 this_file_looks_odd = True
         if this_file_looks_odd:
             any_file_looks_odd = True
